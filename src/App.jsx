253c698a9aee6b7b685bff7061360fddf4cda9de
SujalGop <sujalgopinwar07@gmail.com>
2026-02-25 12:21:30 +0530

    Initial commit: Set up project structure

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import WeatherChecker from './WeatherChecker';
+
+// App simply mounts the WeatherChecker component.
+// All logic, state, and styling live inside WeatherChecker.jsx / WeatherChecker.css.
+function App() {
+  return <WeatherChecker />;
+}
+
+export default App;
