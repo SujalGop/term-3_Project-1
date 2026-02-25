bd3cb7c9081568445231121246c1cb45366ec050
SujalGop <sujalgopinwar07@gmail.com>
2026-02-25 13:31:55 +0530

    Delete src directory

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import WeatherChecker from './WeatherChecker';
-
-// App simply mounts the WeatherChecker component.
-// All logic, state, and styling live inside WeatherChecker.jsx / WeatherChecker.css.
-function App() {
-  return <WeatherChecker />;
-}
-
-export default App;
